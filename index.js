c69ff5dcff4999d331290f3f6e03483ff728a20a
milklot <vbiostflex@gmail.com>
2020-09-09 09:56:57 -0400

    edited errors

--- index.js
+++ index.js
@@ -90,8 +90,7 @@ function feeder(dogWeight, age) {
             return (dogWeight * 0.1);
         } else if ((age >= 0.4) && (age < 0.66)) {
             return (dogWeight * 0.05);
-        }
-        ((age >= 0.67) && (age < 1)) {
+        } else if ((age >= 0.67) && (age < 1)) {
             return (dogWeight * 0.04);
         }
 
@@ -129,104 +128,104 @@ function game(choice) {
         console.log("you win!");
     } else if ((choice == r) && (enemy_choice == p)) {
         console.log("you lost");
-    } else if ((choice == p) && (enemy_choice == p) {
-            console.log("draw");
-        } else if ((choice == p) && (enemy_choice == r)) {
-            console.log("you win!");
-        } else if ((choice == p) && (enemy_choice == s)) {
-            console.log("you lost");
-        } else if ((choice == s) && (enemy_choice == s) {
-                console.log("draw");
-            } else if ((choice == s) && (enemy_choice == p)) {
-                console.log("you win!");
-            } else if ((choice == s) && (enemy_choice == r)) {
-                console.log("you lost");
-            }
+    } else if ((choice == p) && (enemy_choice == p)) {
+        console.log("draw");
+    } else if ((choice == p) && (enemy_choice == r)) {
+        console.log("you win!");
+    } else if ((choice == p) && (enemy_choice == s)) {
+        console.log("you lost");
+    } else if ((choice == s) && (enemy_choice == s)) {
+        console.log("draw");
+    } else if ((choice == s) && (enemy_choice == p)) {
+        console.log("you win!");
+    } else if ((choice == s) && (enemy_choice == r)) {
+        console.log("you lost");
+    }
 
-        }
-
-
-        /************************************************************** Task 5 **************************************************************/
-        //Metric Converter
-        //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
-
-        function distance_converter(kmt) {
-            return (kmt / 1.6);
-        }
+}
 
 
+/************************************************************** Task 5 **************************************************************/
+//Metric Converter
+//a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
-        //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-
-        function feet_to_sm(feet) {
-            return (feet * 30.48);
-        }
+function distance_converter(kmt) {
+    return (kmt / 1.6);
+}
 
 
 
-        /************************************************************** Task 6 **************************************************************/
-        // 99 bottles of soda on the wall
-        // create a function called annoyingSong
-        // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
+//b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
 
-        function annoyingSong(starting_number) {
-            var i = 0;
-            var annoy = starting_number;
-
-            while (i != starting_number) {
-                console.log("bottles of coke on the wall", annoy);
-                i++;
-                annoy--;
-            }
-
-
-        }
-
-        annoyingSong(99);
+function feet_to_sm(feet) {
+    return (feet * 30.48);
+}
 
 
 
-        /************************************************************** Task 7 **************************************************************/
-        //Grade Calculator
-        //write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
-        //90s should be A 
-        //80s should be B 
-        //70s should be Cs 
-        //60s should be D 
-        //and anything below 60 should be F
+/************************************************************** Task 6 **************************************************************/
+// 99 bottles of soda on the wall
+// create a function called annoyingSong
+// the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
 
-        var mark = (Math.floor(Math.random() * 101));
+function annoyingSong(starting_number) {
+    var i = 0;
+    var annoy = starting_number;
 
-        function mark_grade(mark) {
-            if (mark >= 90) {
-                return ("A");
-            } else if ((mark >= 80) && (mark < 90)) {
-                return ("B");
-            } else if ((mark >= 70) && (mark < 80)) {
-                return ("C");
-            } else if ((mark >= 60) && (mark < 70)) {
-                return ("D");
-            } else {
-                return ("F");
-            }
+    while (i != starting_number) {
+        console.log("bottles of coke on the wall", annoy);
+        i++;
+        annoy--;
+    }
 
-        }
 
-        console.log(mark_grade(mark));
+}
+
+annoyingSong(99);
 
 
 
+/************************************************************** Task 7 **************************************************************/
+//Grade Calculator
+//write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade 
+//90s should be A 
+//80s should be B 
+//70s should be Cs 
+//60s should be D 
+//and anything below 60 should be F
 
+var mark = (Math.floor(Math.random() * 101));
 
-        /************************************************************** Stretch **************************************************************/
-        //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
-        // Hint - you may need to study tomorrow's traning kit on arrays 
-        // try looking up the .includes() method
+function mark_grade(mark) {
+    if (mark >= 90) {
+        return ("A");
+    } else if ((mark >= 80) && (mark < 90)) {
+        return ("B");
+    } else if ((mark >= 70) && (mark < 80)) {
+        return ("C");
+    } else if ((mark >= 60) && (mark < 70)) {
+        return ("D");
+    } else {
+        return ("F");
+    }
+
+}
+
+console.log(mark_grade(mark));
 
 
 
 
 
-        /************************************************************** Stretch **************************************************************/
-        //Take Rock, Paper, Sissors further
-        //update your rock papers sissors code below to take a prompt from a user using the window object
+/************************************************************** Stretch **************************************************************/
+//Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
+// Hint - you may need to study tomorrow's traning kit on arrays 
+// try looking up the .includes() method
+
+
+function vowel_count(wor)
+
+
+/************************************************************** Stretch **************************************************************/
+//Take Rock, Paper, Sissors further
+//update your rock papers sissors code below to take a prompt from a user using the window object
